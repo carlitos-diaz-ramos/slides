383ff7eaf179ddd23736b6897161677cd092736e
Carlitos <carlitos.diaz.ramos@gmail.com>
2023-11-14 20:26:37 +0100

    Add boxes with arrows

--- modules/boxes.js
+++ modules/boxes.js
@@ -1,6 +1,29 @@
-export function iff_template() {
-    const tmpl = document.createElement('template');
-    tmpl.innerHTML = `
+class TwoBoxes extends HTMLElement {
+    connectedCallback() {
+        this.attachShadow({mode: 'open'});
+        const template = this.constructor.template(this.dataset.bg);
+        this.shadowRoot.append(template.content.cloneNode(true));
+    }
+
+    static template(color) {
+        const tmpl = document.createElement('template');
+        tmpl.innerHTML = this.prototype.constructor._CODE;
+        this._set_color(tmpl, color);
+        return tmpl;
+    }
+
+    static _set_color(tmpl, color) {
+        const bg = `bg-${color}`;
+        const divs = tmpl.content.querySelectorAll('.two-boxes div');
+        for (let div of divs) 
+            div.classList.add(bg);
+        const path = tmpl.content.querySelector('.two-boxes svg path');
+        path.classList.add(bg);
+    }
+}
+
+export class SlideIff extends TwoBoxes {
+    static _CODE = `
         <link rel="stylesheet" href="../code/slides.css">
         <div class="two-boxes">
          <div>
@@ -17,17 +40,48 @@ export function iff_template() {
          </div>
         </div>
     `;
-    return tmpl;
 }
 
-class SlideIff extends HTMLElement {
-    connectedCallback() {
-        this.attachShadow({mode: 'open'});
-        const tmpl = iff_template();
-        this.shadowRoot.append(tmpl.content.cloneNode(true));
-    }
+export class SlideImplies extends TwoBoxes {
+    static _CODE = `
+        <link rel="stylesheet" href="../code/slides.css">
+        <div class="two-boxes">
+         <div>
+          <slot name="left"></slot>
+         </div>
+         <svg viewBox="0,0 40,22">
+          <title>If and only if</title>
+          <path d="M1,11 V6 H29 V2 Q29,1 29.71,1.70 L39,11 29.71,20.30 
+           Q29,21 29,20 V16 H1 V11 z"/>
+         </svg>
+         <div>
+          <slot name="right"></slot>
+         </div>
+        </div>
+    `;
+}
+
+export class SlideImplied extends TwoBoxes {
+    static _CODE = `
+        <link rel="stylesheet" href="../code/slides.css">
+        <div class="two-boxes">
+         <div>
+          <slot name="left"></slot>
+         </div>
+         <svg viewBox="0,0 40,22">
+          <title>If and only if</title>
+          <path d="M10,6 Q22,20 33,1 L33,8 39,9 Q22,29 5,11 L1,15 L1,2 
+           L14,2 z"/>
+         </svg>
+         <div>
+          <slot name="right"></slot>
+         </div>
+        </div>
+    `;
 }
 
 export function define_elements() {
     customElements.define('slide-iff', SlideIff);
+    customElements.define('slide-implies', SlideImplies);
+    customElements.define('slide-implied', SlideImplied);
 }
--- tests/boxes.js
+++ tests/boxes.js
@@ -1,50 +1,143 @@
-import {iff_template} from "../modules/boxes.js";
+import {SlideIff, SlideImplies, SlideImplied} from "../modules/boxes.js";
 
 describe('Boxes with arrows', function() {
-    describe('iff_template', function() {
+    describe('SlideIff.template', function() {
         it('A div.two-boxes is created', function() {
-            const template = iff_template();
+            const template = SlideIff.template();
             const boxes = template.content.querySelector('div.two-boxes');
             assert.notEqual(boxes, null);
         })
 
         it('div.two-boxes has two divs', function() {
-            const template = iff_template();
-            const boxes = template.content.querySelector('div.two-boxes');
-            const divs = boxes.querySelectorAll('div');
+            const template = SlideIff.template();
+            const divs = template.content.querySelectorAll('.two-boxes div');
             assert.equal(divs.length, 2);
         })
 
         it('1st div in .two-boxes has a slot with name "left"', function() {
-            const template = iff_template();
-            const boxes = template.content.querySelector('div.two-boxes');
-            const divs = boxes.querySelectorAll('div');
+            const template = SlideIff.template();
+            const divs = template.content.querySelectorAll('.two-boxes div');
             const slots = divs[0].querySelectorAll('slot[name="left"');
             assert(slots.length, 1);
         })
 
         it('2nd div in .two-boxes has a slot with name "right"', function() {
-            const template = iff_template();
-            const boxes = template.content.querySelector('div.two-boxes');
-            const divs = boxes.querySelectorAll('div');
+            const template = SlideIff.template();
+            const divs = template.content.querySelectorAll('.two-boxes div');
             const slots = divs[1].querySelectorAll('slot[name="right"');
             assert(slots.length, 1);
         })
 
         it('A link is created', function() {
-            const template = iff_template();
+            const template = SlideIff.template();
             const link = template.content.querySelector('link');
             assert.notEqual(link, null);
         })
 
         it('link refers to slides.css', function() {
-            const template = iff_template();
+            const template = SlideIff.template();
             const link = template.content.querySelector('link');
             assert.equal(link.href.slice(-16), '/code/slides.css');
         })
 
         it('There is a svg between the divs', function() {
-            const template = iff_template();
+            const template = SlideIff.template();
+            const svg = template.content.querySelector('div.two-boxes svg');
+            assert.notEqual(svg, null);
+            const previous = svg.previousElementSibling
+            assert.equal(previous.nodeName.toLowerCase(), 'div');
+            const next = svg.nextElementSibling;
+            assert.equal(next.nodeName.toLowerCase(), 'div');
+        })
+
+        it('If color is yellow, divs have class yellow', function() {
+            const template = SlideIff.template('yellow');
+            const divs = template.content.querySelectorAll('.two-boxes div');
+            for (let div of divs) {
+                const cls = Array.from(div.classList);
+                assert.isTrue(cls.includes('bg-yellow'));
+            }
+        })
+
+        it('If color is yellow, path has class yellow', function() {
+            const template = SlideIff.template('yellow');
+            const selector = '.two-boxes svg path';
+            const path = template.content.querySelector(selector);
+            const cls = Array.from(path.classList);
+            assert.isTrue(cls.includes('bg-yellow'));
+        })
+    })
+
+    describe('SlideImplies', function() {
+        it('A div.two-boxes is created', function() {
+            const template = SlideImplies.template();
+            const boxes = template.content.querySelector('div.two-boxes');
+            assert.notEqual(boxes, null);
+        })
+
+        it('div.two-boxes has two divs', function() {
+            const template = SlideImplies.template();
+            const divs = template.content.querySelectorAll('.two-boxes div');
+            assert.equal(divs.length, 2);
+        })
+
+        it('1st div in .two-boxes has a slot with name "left"', function() {
+            const template = SlideImplies.template();
+            const divs = template.content.querySelectorAll('.two-boxes div');
+            const slots = divs[0].querySelectorAll('slot[name="left"');
+            assert(slots.length, 1);
+        })
+
+        it('2nd div in .two-boxes has a slot with name "right"', function() {
+            const template = SlideImplies.template();
+            const divs = template.content.querySelectorAll('.two-boxes div');
+            const slots = divs[1].querySelectorAll('slot[name="right"');
+            assert(slots.length, 1);
+        })
+
+        it('A link is created', function() {
+            const template = SlideImplies.template();
+            const link = template.content.querySelector('link');
+            assert.notEqual(link, null);
+        })
+
+        it('link refers to slides.css', function() {
+            const template = SlideImplies.template();
+            const link = template.content.querySelector('link');
+            assert.equal(link.href.slice(-16), '/code/slides.css');
+        })
+
+        it('There is a svg between the divs', function() {
+            const template = SlideImplies.template();
+            const svg = template.content.querySelector('div.two-boxes svg');
+            assert.notEqual(svg, null);
+            const previous = svg.previousElementSibling
+            assert.equal(previous.nodeName.toLowerCase(), 'div');
+            const next = svg.nextElementSibling;
+            assert.equal(next.nodeName.toLowerCase(), 'div');
+        })
+
+        it('If color is green, divs have class yellow', function() {
+            const template = SlideImplies.template('green');
+            const divs = template.content.querySelectorAll('.two-boxes div');
+            for (let div of divs) {
+                const cls = Array.from(div.classList);
+                assert.isTrue(cls.includes('bg-green'));
+            }
+        })
+
+        it('If color is green, path has class yellow', function() {
+            const template = SlideImplies.template('green');
+            const selector = '.two-boxes svg path';
+            const path = template.content.querySelector(selector);
+            const cls = Array.from(path.classList);
+            assert.isTrue(cls.includes('bg-green'));
+        })
+    })
+
+    describe('SlideImplied', function() {
+        it('There is a svg between the divs', function() {
+            const template = SlideImplied.template();
             const svg = template.content.querySelector('div.two-boxes svg');
             assert.notEqual(svg, null);
             const previous = svg.previousElementSibling
