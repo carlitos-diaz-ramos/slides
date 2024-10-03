5b4fff4aef0cc4d33a39e3009345a7aaf1e5f661
Carlitos <carlitos.diaz.ramos@gmail.com>
2024-10-03 22:48:56 +0200

    Basic scribbling

--- modules/canvas.js
+++ modules/canvas.js
@@ -3,16 +3,101 @@
  */
 
 
+export class CanvasError extends Error {}
+
 export class Canvas {
     constructor(document) {
         this._document = document;
+        this._scribbling = false;
     }
 
     start() {
         this.slide = this._document.querySelector('.current');
-        const svg = this._document.createElement('svg');
+        this._create_svg();
+    }
+
+    set_size(width, height) {
+        this._width = width;
+        this._height = height;
+    }
+
+    static _SVG_NS = "http://www.w3.org/2000/svg";
+
+    _create_svg() {
+        const ns = this.constructor._SVG_NS;
+        const svg = this._svg = this._document.createElementNS(ns, 'svg');
         svg.classList.add('scribble');
-        this.slide.appendChild(svg);
+        this.set_size(this.slide.offsetWidth, this.slide.offsetHeight);
+        svg.setAttribute('width', '100%');
+        svg.setAttribute('height', '100%');
+        svg.setAttribute('viewBox', `0,0 ${this._width},${this._height}`);
+        svg.setAttribute('stroke', 'black');
+        svg.setAttribute('stroke-width', '3');
+        svg.setAttribute('fill', 'none');
+        this._add_listeners();
+        this.slide.append(svg);
+    }
+
+    _add_listeners() {
+        this._svg.addEventListener('mousedown', this._on_mouse_down);
+        this._svg.addEventListener('mousemove', this._on_mouse_move);
+        this._svg.addEventListener('mouseup', this._on_mouse_up);
+        this._document.addEventListener('keydown', this._on_key_down);
+    }
+
+    _on_mouse_down = (event) => {
+        const [x, y] = [event.layerX, event.layerY];
+        this.start_stroke(x, y);
+    }
+
+    _on_mouse_move = (event) => {
+        if (event.buttons == 1) {
+            const [x, y] = [event.layerX, event.layerY];
+            this.continue_stroke(x, y);
+        }
+    }
+
+    _on_mouse_up = (event) => {
+        const [x, y] = [event.layerX, event.layerY];
+        this.end_stroke(x, y);
+    }
+
+    _on_key_down = (event) => {
+        if (event.code == "KeyZ" && event.ctrlKey) {
+            this.undo_last();
+        }
+    }
+
+    start_stroke(x, y) {
+        const ns = this.constructor._SVG_NS;
+        this._scribbling = true;
+        const polyline = this._document.createElementNS(ns, 'polyline');
+        polyline.setAttribute('points', `${x},${y}`);
+        this._svg.append(polyline);
+    }
+
+    continue_stroke(x, y) {
+        if (!this._scribbling) 
+            throw new CanvasError('Stroke has not started yet.')
+        this._add_stroke(x, y);
+    }
+
+    end_stroke(x, y) {
+        this._add_stroke(x, y);
+        this._scribbling = false;
+    }
+
+    _add_stroke(x, y) {
+        const polyline = this._svg.querySelector('polyline:last-child');
+        const previous = polyline.getAttribute('points');
+        const current = previous.concat(' ', `${x},${y}`);
+        polyline.setAttribute('points', current);
+    }
+
+    undo_last() {
+        const polyline = this._svg.querySelector('polyline:last-child');
+        if (polyline !== null)
+            polyline.remove();
     }
 }
 
