40119865aacb6306357a739dc98dd811ab226db4
carlitos.diaz.ramos <carlitos.diaz.ramos@gmail.com>
2025-12-21 20:46:02 +0100

    Set server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 import * as path from 'path';
+// import {EsbuildPlugin} from 'esbuild-loader';
 
 export default {
     mode: 'development',
     entry: './modules/slides.js',
+    devtool: 'inline-source-map',
     output: {
         filename: 'slides.js',
         path: path.resolve('dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(''),
+        },
+        open: [
+            './docs/doc.html',
+        ],
+    },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.[jt]sx?$/,
+    //             loader: 'esbuild-loader',
+    //             options: {
+    //                 target: 'es2015',
+    //             },
+    //         },
+    //     ]
+    // },
+    optimization: {
+        // minimize: true,
+        // minimizer: [
+        //     new EsbuildPlugin({target: 'es2015'}),
+        // ]
     },
 };
